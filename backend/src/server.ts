ab066dee8f4dcabbd447508e35d2750c8664df4a
joandersonSJ <joandersonsantos376@gmail.com>
2020-06-01 10:16:04 -0300

    first commit

---
+++ backend/src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+const app = express();
+
+app.use(express.json())
+
+app.get('/', (req,res) => {
+  res.json({msg: 'hello World'})
+})
+
+app.listen(3333,() => {
+  console.log('Server is running!')
+})
