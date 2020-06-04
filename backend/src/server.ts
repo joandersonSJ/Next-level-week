3c8835e14bb6dd8b63fc0cbbf8d62461937a95fb
joandersonSJ <joandersonsantosdev@gmail.com>
2020-06-04 16:57:49 -0300

    backend has been ended

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,4 @@
-import express from 'express'
-import routes from './routes'
-import path from 'path'
-const app = express()
-
-app.use(express.json())
-app.use(routes)
-app.use('/uploads', express.static(path.resolve(__dirname, '.', 'tmp')))
+import app from './App'
 
 app.listen(3333, () => {
   console.log('Server is running!')
